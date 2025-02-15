3a20025781ff8b166d3e22ff45534ccfd7026976
Sergej Chodarev <sergejx@sergejx.net>
2025-02-15 21:11:51 +0100

    Update Gradle configuration

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 group = "sk.tuke.meta"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
@@ -13,6 +18,7 @@ dependencies {
     implementation(project(":persistence"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -5,15 +5,20 @@ plugins {
 group = "sk.tuke.meta"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    api("org.xerial:sqlite-jdbc:3.41.2.2")
+    api("org.xerial:sqlite-jdbc:3.49.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
