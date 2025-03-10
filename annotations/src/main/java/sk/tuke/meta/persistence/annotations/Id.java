86aa4a8d248ce972d75233df630a804c26212431
Martin Mormak <martin.mormak@student.tuke.sk>
2025-03-10 11:53:04 +0100

    feat: added target into annotations

--- annotations/src/main/java/sk/tuke/meta/persistence/annotations/Id.java
+++ annotations/src/main/java/sk/tuke/meta/persistence/annotations/Id.java
@@ -1,4 +1,8 @@
 package sk.tuke.meta.persistence.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
 public @interface Id {
 }
--- annotations/src/main/java/sk/tuke/meta/persistence/annotations/Table.java
+++ annotations/src/main/java/sk/tuke/meta/persistence/annotations/Table.java
@@ -1,5 +1,9 @@
 package sk.tuke.meta.persistence.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface Table {
     String name() default "";
 }
